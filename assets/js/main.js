07ac49e803101dbf25d0c3656b432b368e7eb72b
PRNVBAJAJ <pranavbajaj4969@gmail.com>
2021-05-11 20:37:30 +0530

    changed smooth scrolling function

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,11 +116,22 @@
   });
 
   //Smooth Scrolling Function
-  var scroll = new SmoothScroll('.site-navigation a[href*="#"]', {
-    speed: 800,
-    speedAsDuration: false,
-    easing: 'easeInOutCubic'
-  });
+  // var scroll = new SmoothScroll('.site-navigation a[href*="#"]', {
+  //   speed: 800,
+  //   speedAsDuration: false,
+  //   easing: 'easeInOutCubic'
+  // });
+
+  $('.site-navigation a').on('click',function(e){
+    if(this.hash !== ''){
+      e.preventDefault();
+      const hash = this.hash;
+      $('html,body').animate({
+        scrollTop: $(hash).offset().top
+      },
+      800);
+    }
+  })
 
 
 })(jQuery);
