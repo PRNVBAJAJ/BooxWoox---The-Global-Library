553b4a5d243a9405cb3ea3c5b5f8c4e94365a786
PRNVBAJAJ <pranavbajaj4969@gmail.com>
2021-05-12 20:26:22 +0530

    Updated illustrations, changed active link function
    Updated illustrations for buy, lend and borrow.
    Changed active link js to run according to scrolls.

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,15 +122,35 @@
   //   easing: 'easeInOutCubic'
   // });
 
-  $('.site-navigation a').on('click',function(e){
-    if(this.hash !== ''){
+  // 
+  $(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
       e.preventDefault();
-      const hash = this.hash;
-      $('html,body').animate({
-        scrollTop: $(hash).offset().top
-      },
-      800);
-    }
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+      
+      scrollLink.each(function() {
+        
+        var sectionOffset = $(this.hash).offset().top - 100;
+        
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+        }
+      })
+      
+    })
+    
   })
 
 
